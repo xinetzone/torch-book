4af73f5b632cfee67b00a42c64fd44429dec2ef0
liuxinwei <liuxinwei@xmsilicon.cn>
2022-10-27 21:19:25 +0800

    更新

--- doc/tutorial/quant/sigmoid-fx/fit_sigmoid.py
+++ doc/tutorial/quant/sigmoid-fx/fit_sigmoid.py
@@ -8,25 +8,26 @@ def linear(X, X0, X1, Y0, Y1):
     l1 = (X-X0)/(X1 - X0)
     return l0 * Y0 + l1 * Y1
 
-def get_nodes(features, n_samples=10000, bins=10):
+def get_nodes(features, bins=10):
     # X = torch.randn(n_samples, 16, 4, 4)
-    full_index = torch.sigmoid(features).argsort(dim=0)
-    index = full_index[::n_samples//bins]
-    return torch.concat([features.take(index), 
-                         features.take(full_index[-1]).unsqueeze(dim=0)])
+    n_samples = len(features)
+    step = n_samples//bins
+    assert step > 0, "校准数据不匹配"
+    full_index = features.argsort(dim=0)
+    index = full_index[::step]
+    index = torch.concat([full_index[::step],
+                          full_index[-1].unsqueeze(dim=0)])
+    return features.take(index)
 
-def _sigmoid2linear(X, n_samples=10000, bins=10):
-    Xs = get_nodes(X, n_samples=n_samples, bins=bins)
-    bins = len(Xs) - 1
+def _sigmoid2linear(X, bins=10):
+    Xs = get_nodes(X, bins=bins)
     Ys = torch.sigmoid(Xs)
-    Y = torch.zeros_like(X)
+    Y = torch.zeros_like(X) * (X < -6)
     for k in range(bins):
-        interval = (X >= Xs[k]) * (X <= Xs[k+1])
-        Y += linear(X, Xs[k], Xs[k+1], Ys[k], Ys[k+1]) # interval
+        interval = (X >= Xs[k]) * (X <= Xs[k+1]) + (X <= Xs[k]) * (X >= Xs[k+1])
+        Y += linear(X, Xs[k], Xs[k+1], Ys[k], Ys[k+1]) * interval
     return Y
 
-def sigmoid2linear(Xs, n_samples=10000, bins=10):
-    return torch.stack([_sigmoid2linear(x, 
-                                        n_samples=n_samples, 
-                                        bins=bins) 
+def sigmoid2linear(Xs, bins=10):
+    return torch.stack([_sigmoid2linear(x, bins=bins) 
                         for x in Xs])
